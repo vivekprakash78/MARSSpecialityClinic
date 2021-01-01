666c435c79c3d8fa22df673658b9b063bbc12022
Vivek Prakash <vivekprakash78@gmail.com>
2021-01-02 01:06:02 +0530

    Fix UTs and warnings

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Hospital Address test', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Shaguna More/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Hospital Phone number', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/91 860 334 8924/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Hospital Subtext', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Passion for Cure/i);
   expect(linkElement).toBeInTheDocument();
 });
